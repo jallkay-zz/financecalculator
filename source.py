21ffeedb70be157e2dfc4c57b34cda5ee2d40558
James Allington-Kay <lancaster.1@hotmail.co.uk>
2018-05-28 13:48:34 +0100

    initial comments

---
+++ source.py
@@ -0,0 +1,13 @@
+#import stuff
+
+# pull data in from eBay
+# pull in number plate data
+# pull in all different models of cars
+
+# group cars together using some matching algorithms - Querying the number plate would clean it up
+
+# perform polynomial regression on the grouped cars
+# either do this at runtime or pre-record it and display the curves
+
+
+# output data
